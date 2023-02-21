38b365ce552c03551fb93fb32028fc6fe83be9ff
Mariglen Jahaj <31398575+mJahaj@users.noreply.github.com>
2023-02-21 01:29:54 -0500

    Enabled User input functionality
    user can now enter a goal and click enter to add it to the goals list

--- App.js
+++ App.js
@@ -1,15 +1,44 @@
-
-import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
+import { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  TextInput,
+  ScrollView,
+  FlatList
+} from "react-native";
 
 export default function App() {
+  const [enteredGoalText, setEnteredGoalText] = useState("");
+  const [allGoals, setAllGoals] = useState([]);
+
+  function goalInputHandler(enteredText) {
+    setEnteredGoalText(enteredText);
+  }
+
+  function addGoalHandler() {
+    setAllGoals((currentGoals) => [...currentGoals, enteredGoalText]);
+  }
+
   return (
     <View style={styles.appContainer}>
-      <View style={styles.inputContainer}> 
-        <TextInput style={styles.textInput} placeholder='Your course goal!'/>
-        <Button title='Add Goal'/>
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.textInput}
+          placeholder="Your course goal!"
+          onChangeText={goalInputHandler}
+        />
+        <Button title="Add Goal" onPress={addGoalHandler} />
       </View>
-      <View> 
-        <Text>List of goals...</Text>
+      <View style={styles.goalsContainer}>
+        <ScrollView>
+          {allGoals.map((goal) => (
+            <View style={styles.goalItem} key={goal}>
+              <Text style={styles.goalText}>{goal}</Text>
+            </View>
+          ))}
+        </ScrollView>
       </View>
     </View>
   );
@@ -17,18 +46,36 @@ export default function App() {
 
 const styles = StyleSheet.create({
   appContainer: {
-    padding: 50,
+    paddingTop: 50,
+    paddingHorizontal: 16,
+    flex: 1,
   },
   inputContainer: {
-    flexDirection: 'row',
-    justifyContent: 'space-between'
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginBottom: 24,
+    borderBottomWidth: 1,
+    borderBottomColor: "#cccccc",
   },
   textInput: {
     borderWidth: 1,
-    borderColor: '#cccccc',
-    width: '80%',
+    borderColor: "#cccccc",
+    width: "70%",
     marginRight: 8,
     padding: 8,
-  }
-
+  },
+  goalsContainer: {
+    flex: 5,
+  },
+  goalItem: {
+    margin: 8,
+    borderRadius: 6,
+    backgroundColor: "#5e0acc",
+    padding: 8,
+  },
+  goalText: {
+    color: "white",
+  },
 });
--- README.md
+++ README.md
@@ -7,3 +7,20 @@
 
  Run `npm install` to install all dependencies for the project
  `npm start` to start application
+
+ #CSS
+ flexbox - turned on by default
+ flexDirection , row col reverse etc
+
+ justifyContent: ' '- organize elements along main axis
+ alignItems: ' ' - organize elements on cross axis
+
+ flex - applied to items inside a flexbox
+ flex: 1 - item takes as much space as it can
+
+ #Handle Events like click button
+
+ #states
+
+ if your new state depends on a previous state its best to pass a function(state update function)
+ function called automaticaly by react
